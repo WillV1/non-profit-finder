d2bfd39065a2e764dfc40f0387a510d80f1b5da4
WillV1 <wmvanhook@gmail.com>
2020-04-27 18:00:18 -0400

    Uploading changes to html page and JS search/API call functions

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
 $(document).ready(function () {
 
-    var state;
-    var city;
-    var keyword;
+    var searchVal;
 
     M.AutoInit();
     $(document).ready(function () {
         $('select').formSelect();
     });
-
-    $('#textarea1').val('New Text');
     M.textareaAutoResize($('#textarea1'));
 
 
     //Build in Javascript that allows dropdown option to determine what text will go into text area
 
-    $('#organization-search').on('change', function () {
-        $('#textarea1').val($('#organization-search option:selected').val());
+    var myTextBox = $('#textarea1');
+    var myDropDown = $('#organization-search');
 
-        $('#textarea1').val('');
-
-        if ($('#organization-search option:selected').val("1")) {
-            var city = $('#textarea1');
-            city.val($(this)[0].value);
-            console.log(city);
-            // } else if ($('#organization-search option:selected').val() == "2") {
-            //     var state = $(this).val();
-            //     console.log(state);
-            // } else if ($('#organization-search option:selected').val() == "3") {
-            //     var keyword = $(this).val();
-            //     console.log(keyword)
-            // } else {
-            //     return 
-        }
-
-    });
+    myDropDown.on('change', function(){
+         searchVal = ($(this).val()); 
+        console.log($(this).val())
+    })    
 
 
     //Build in Javascript for Charity Navigator API calls, based on name, city, state or keyword
+    $('#button').on('click', function(event){
+        event.preventDefault();
+        var searchParam = myTextBox.val();
+        search(searchVal, searchParam);
+    })
 
-
-    function search(state, city, keyword) {
+    function search(searchVal, searchParam) {
         $('.large').html(' ');
 
-        var queryURL = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=f23e3059&app_key=e0734aa01e43908655ef9c264f6dcf2e&pageSize=10&search=' + keyword + '&state=' + state + '&city=' + city;
+        var baseURL = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=f23e3059&app_key=e0734aa01e43908655ef9c264f6dcf2e'
+        var endingURL;
+
+        if (searchVal == 'city'){
+           endingURL = '&city=' + searchParam;
+        } else {
+            endingURL = '&search=' + searchParam;
+        }
+
 
         //Build in Javascript for API call to coincide with search parameters 
 
         $.ajax({
-            url: queryURL,
+            url: baseURL + endingURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
