0d5b19c8873161162bd6622eaf7e96a28c732724
WillV1 <wmvanhook@gmail.com>
2020-04-26 22:34:03 -0400

    Committing initial homepage and JS file to GitHub

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+M.AutoInit();
+$('.dropdown-trigger').dropdown();
+
+//Build in Javascript for Charity Navigator API calls, based on name, home country or countries served
+
+var url = 'https://api.data.charitynavigator.org/v2'
+
+//Build in Javascript for API call to coincide with search parameters 
+
+//Build in Javascript for card to display charity information based on API call
+
+// Build in functionality so that user can click link to go to organization's home page
+
+});
