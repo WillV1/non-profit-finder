8ed90034f0d2e249dbe400111de417b06cb1a49d
WillV1 <wmvanhook@gmail.com>
2020-04-27 22:01:33 -0400

    Updating repo for updated organization selections

--- script.js
+++ script.js
@@ -14,14 +14,15 @@ $(document).ready(function () {
     var myTextBox = $('#textarea1');
     var myDropDown = $('#organization-search');
 
-    myDropDown.on('change', function(){
-         searchVal = ($(this).val()); 
+    myDropDown.on('change', function () {
+        searchVal = ($(this).val());
         console.log($(this).val())
-    })    
+    })
 
 
     //Build in Javascript for Charity Navigator API calls, based on name, city, state or keyword
-    $('#button').on('click', function(event){
+    //Build in Javascript for API call to coincide with search parameters 
+    $('#button').on('click', function (event) {
         event.preventDefault();
         var searchParam = myTextBox.val();
         search(searchVal, searchParam);
@@ -33,27 +34,49 @@ $(document).ready(function () {
         var baseURL = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=f23e3059&app_key=e0734aa01e43908655ef9c264f6dcf2e'
         var endingURL;
 
-        if (searchVal == 'city'){
-           endingURL = '&city=' + searchParam;
+        if (searchVal == 'city') {
+            endingURL = '&city=' + searchParam;
+        } else if (searchVal == 'state') {
+            endingURL = '&state=' + searchParam;
         } else {
             endingURL = '&search=' + searchParam;
         }
-
-
-        //Build in Javascript for API call to coincide with search parameters 
 
         $.ajax({
             url: baseURL + endingURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-        });
 
+            //var orgArray = [];
+
+            for (var i = 0; i <= 10; i++) {
+                var orgList = ([Math.floor(Math.random() * response[i].charityName.length)]);
+                console.log(orgList);
+                var orgDisplay = $('<p>');
+                orgDisplay.text(response[i].charityName);
+                orgDisplay.attr("id", "charity-name");
+                orgWebsiteOne = $("<a>");
+                orgWebsiteOne.attr("id", "website");
+                orgWebsiteOne.attr("href", response[i].charityNavigatorURL);
+                orgWebsiteOne.attr("target", "_blank");
+                orgWebsiteOne.text(response[i].charityName);
+                orgWebsiteOne.addClass("link");
+                $('.large').html(orgDisplay);
+                $('#charity-name').append(orgWebsiteOne);
+            }
+
+        });
 
         //Build in Javascript for card to display charity information based on API call
 
 
+
+
         // Build in functionality so that user can click link to go to organization's home page
+
+
+        //Allow for local storage of previous 5 searches
 
     };
 
