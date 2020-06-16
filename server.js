91ae7923bea1b33b10b6fb8805a54f17ecd88c3b
WillV1 <wmvanhook@gmail.com>
2020-06-16 13:44:04 -0400

    Update server.js file for style.css path

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const consign = require('consign');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
 consign()
   .include("routes")
